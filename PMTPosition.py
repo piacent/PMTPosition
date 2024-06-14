6bdb404bf7b213da1d4411cc12289b62cf7b5fdc
piacent <stefano.piacentini@hotmail.com>
2024-06-14 11:42:33 +0200

    function to retrive distance between GEM plane and PMT plane, function to compute geometic distance between PMT and events on the GEM plane

--- PMTPosition.py
+++ PMTPosition.py
@@ -12,6 +12,13 @@ def GetGEMsDim():
     GEM_height = 80.0
     return GEM_width, GEM_height
 
+def GetGEMsZ():
+    """
+    Returns z distance GEM distance form PMT in cm 
+    """
+    GEM_z = 60.0 # FIXME: put here the real number!!!
+    return GEM_z
+
 def GenEventPosition(x_offset = 0.0, y_offset = 0.0, size = 10, distribution = 'uniform'):
     """
     Returns N=size randomly generated spot positions with the specified spacial `distribution`, and
@@ -28,8 +35,19 @@ def GenEventPosition(x_offset = 0.0, y_offset = 0.0, size = 10, distribution = '
     
     return pos
 
+def GetR(PMT_pos = np.array([0.0, 0.0]), Event_pos = np.array([0.0, 0.0])):
+    """
+    Returns distance between events and the PMT in cm, given the PMT position (PMT_pos) and the position of the events (Event_pos)
+    """
+    GEM_z = GetGEMsZ()
+    R     = np.sqrt(GEM_z**2 + (PMT_pos[0] - Event_pos.T[0])**2 + (PMT_pos[1] - Event_pos.T[1])**2)
+    return R
+
 
 if __name__ == "__main__":
     
     pos = GenEventPosition()
     print(pos)
+
+    R   = GetR(PMT_pos = np.array([25.0, 40.0]), Event_pos = pos)
+    print(R)
