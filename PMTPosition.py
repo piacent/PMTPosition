f0d6b4ce10b17aef7c385ae40ca0a45356ba33fb
piacent <stefano.piacentini@hotmail.com>
2024-06-14 11:26:27 +0200

    generation of rando xy position on the GEM plain

--- PMTPosition.py
+++ PMTPosition.py
@@ -6,8 +6,24 @@ import matplotlib.pyplot as plt
 
 def GetGEMsDim():
     """
-    Return GEM dimensions in cm (width and height, respectively)
+    Returns GEM dimensions in cm (width and height, respectively)
     """
     GEM_width  = 50.0
     GEM_height = 80.0
     return GEM_width, GEM_height
+
+def GenEventPosition(x_offset = 0.0, y_offset = 0.0, size = 10, distribution = 'uniform'):
+    """
+    Returns N=size randomly generated spot positions with the specified spacial `distribution`, and
+    assuming an offset = [x_offset, y_offset]
+    """
+    GEM_width, GEM_height = GetGEMsDim()
+    if distribution == 'uniform':
+        x = st.uniform.rvs(loc = x_offset, scale = GEM_width,  size = size)
+        y = st.uniform.rvs(loc = y_offset, scale = GEM_height, size = size)
+    else:
+        raise Exception("PMTPosition.GenEventPosition: unknown '"+distribution+"' distribution.")
+
+    pos = np.vstack((x, y)).T
+    
+    return pos
