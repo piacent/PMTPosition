2090651cdb8471e3dfee2d0604577720e02b9a44
piacent <stefano.piacentini@hotmail.com>
2024-06-14 11:55:05 +0200

    function to compute geometic distance between all the PMTs and events on the GEM plane

--- PMTPosition.py
+++ PMTPosition.py
@@ -35,7 +35,7 @@ def GenEventPosition(x_offset = 0.0, y_offset = 0.0, size = 10, distribution = '
     
     return pos
 
-def GetR(PMT_pos = np.array([0.0, 0.0]), Event_pos = np.array([0.0, 0.0])):
+def GetR(PMT_pos = np.array([0.0, 0.0]), Event_pos = np.array([[0.0, 0.0]])):
     """
     Returns distance between events and the PMT in cm, given the PMT position (PMT_pos) and the position of the events (Event_pos)
     """
@@ -43,11 +43,32 @@ def GetR(PMT_pos = np.array([0.0, 0.0]), Event_pos = np.array([0.0, 0.0])):
     R     = np.sqrt(GEM_z**2 + (PMT_pos[0] - Event_pos.T[0])**2 + (PMT_pos[1] - Event_pos.T[1])**2)
     return R
 
+def GetR_All(PMT_pos = np.array([[0.0, 0.0]]), Event_pos = np.array([[0.0, 0.0]])):
+    """
+    Returns distance between events and all the PMTs in cm, given the PMT positions (PMT_pos) and the position of the events (Event_pos)
+    """
+    R_all = np.array([])
+    for pmt_xy in PMT_pos:
+        if len(R_all) == 0:
+            R_all = GetR(PMT_pos = pmt_xy, Event_pos = Event_pos)
+        else:
+            R_all = np.vstack((R_all, GetR(PMT_pos = pmt_xy, Event_pos = Event_pos)))
+    return R_all.T
+    
+
+
 
 if __name__ == "__main__":
-    
+
+    print("\nPositions:")
     pos = GenEventPosition()
     print(pos)
 
+    print("\nR:")
     R   = GetR(PMT_pos = np.array([25.0, 40.0]), Event_pos = pos)
     print(R)
+
+    print("\nR_all:")
+    PMT_pos = np.array([[25.0, 20.0], [25.0, 60.0]])
+    R_all   = GetR_All(PMT_pos = PMT_pos, Event_pos = pos)
+    print(R_all)
